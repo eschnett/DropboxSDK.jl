668376be088c6cd6f7920a9496f51ef82e44e7ca
Erik Schnetter <schnetter@gmail.com>
2019-03-18 12:28:56 -0400

    Avoid colons in time stamp

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,8 @@ using UUIDs
 
 
 
-const timestamp = Dates.format(now(UTC), dateformat"yyyy-mm-ddTHH:MM:SS.sss")
+# Avoid colons in the timestamp
+const timestamp = Dates.format(now(UTC), dateformat"yyyy-mm-dd-HH-MM-SS.sss")
 const uuid = UUIDs.uuid4()
 const folder = "test-$timestamp-$uuid"
 println("Using folder \"$folder\" for testing")
